8b9f803942213847d9eefa28cc36aec33647df26
ProWeb4All <proweb4all@narod.ru>
2020-05-13 19:57:58 +0300

    update

--- src/components/selected-item/SelectedItem.js
+++ src/components/selected-item/SelectedItem.js
@@ -4,7 +4,7 @@ import "./selected-item.scss";
 const SelectedItem = (props) => {
   return (
     <div className='selected-item'>
-      <span className='selected-item__name'>{props.id}-{props.name}</span>
+      <span className='selected-item__name'>{props.name}</span>
       <div className='selected-item__close' onClick={props.unselectItem}>✖</div>
     </div>
   )
